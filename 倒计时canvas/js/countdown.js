72f0e676a4462472ac2e92bf957025ffb5a560cc
Dida-1209 <18814127315@163.com>
2016-05-16 13:33:56 +0800

    Time

--- 倒计时canvas/js/countdown.js
+++ 倒计时canvas/js/countdown.js
@@ -4,6 +4,9 @@ var RADIUS=8;
 var MARGIN_TOP=60;
 var MARGIN_LEFT=30;
 
+const endTime = new Date(2016,4,17,13,15,20);
+var curShowTimeSeconds=0;
+
 window.onload=function(){
 	
 	var canvas=document.getElementById('canvas');
@@ -12,13 +15,21 @@ window.onload=function(){
 	canvas.width=WINDOW_WIDTH;
 	canvas.height=WINDOW_HEIGHT;
 	
+	curShowTimeSeconds=getCurrentShowTimeSeconds();
 	render(context)
 }
 
+function getCurrentShowTimeSeconds(){
+	var curTime = new Date();
+	var ret=endTime.getTime()-curTime.getTime();
+	ret=Math.round(ret/1000);
+	return ret>=0?ret:0;
+}
+
 function render(cxt){
-	var hours=12;
-	var minutes=34;
-	var seconds=56;
+	var hours=parseInt(curShowTimeSeconds/3600);
+	var minutes=parseInt((curShowTimeSeconds-hours*3600)/60);
+	var seconds=curShowTimeSeconds%60;
 	
 	renderDigit(MARGIN_LEFT,MARGIN_TOP,parseInt(hours/10),cxt);
 	renderDigit(MARGIN_LEFT+15*(RADIUS + 1),MARGIN_TOP,parseInt(hours%10),cxt);
@@ -37,7 +48,7 @@ function renderDigit(x,y,num,cxt){
 	for(var j=0;j<digit[num][i].length;j++)
 	if(digit[num][i][j]==1){
 		cxt.beginPath();
-		cxt.arc();
+		cxt.arc(x+j*2*(RADIUS+1)+(RADIUS+1),y+i*2*(RADIUS+1)+(RADIUS+1),RADIUS,0,2*Math.PI);
 		cxt.closePath();
 		
 		cxt.fill();
