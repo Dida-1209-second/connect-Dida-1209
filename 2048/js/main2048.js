188e44aea5143f7cf9f82cf0819bb233a963d92a
Dida-1209 <18814127315@163.com>
2016-06-01 14:01:06 +0800

    生成数字

--- 2048/js/main2048.js
+++ 2048/js/main2048.js
@@ -10,6 +10,9 @@ function newgame(){
 	//初始化棋盘格
 	init();
 	//在随机两个格子生成数字
+	generateOneNumber();
+	generateOneNumber();
+	
 }
 
 function init(){
@@ -30,5 +33,44 @@ function init(){
 function updateBoardView(){
 	$(".number-cell").remove();
 	for(var i=0;i<4;i++)
-	
+		for(var j=0;j<4;j++){
+			$("#grid-container").append('<div class="number-cell" id="number-cell-"'+i+"-"+j+'></div>');
+			var theNumberCell=$('#number-cell-'+i+'-'+j);
+			
+			if(board[i][j]==0){
+				theNumberCell.css('width','0px');
+				theNumberCell.css('height','0px');
+				theNumberCell.css('top',getPosTop(i,j)+50);
+				theNumberCell.css('left',getPosLeft(i,j)+50);
+			}else{
+				theNumberCell.css('width','100px');
+				theNumberCell.css('height','100px');
+				theNumberCell.css('top',getPosTop(i,j));
+				theNumberCell.css('left',getPosLeft(i,j));
+				theNumberCell.css('background-color',getNumberBackgroundColor(board[i][j]));
+				theNumberCell.css('color',getNumberColor(board[i][j]));
+				theNumberCell.text(board[i][j]);
+			}
+		}
 }
+function generateOneNumber(){
+	if(nospace(board))
+		return false;
+		
+	//随机位置
+	var randx=parseInt(Math.floor(Math.random()*4));
+	var randy=parseInt(Math.floor(Math.random()*4));
+	while(true){
+		if(board[randx][randy]==0)
+			break;
+		randx=parseInt(Math.floor(Math.random()*4));
+		randy=parseInt(Math.floor(Math.random()*4));
+	}
+	//随机数字
+	var randNumber=Math.random()<0.5?2:4;
+	board[randx][randy]=randNumber;
+	showNumberWithAnimation(randx,randy,randNumber);
+	return true;
+}
+
+
